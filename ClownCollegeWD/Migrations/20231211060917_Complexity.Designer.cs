3c30789bbf5e8222b62466345540ac65d7c6c33c
mason-pAoLo <masonmillionaire725@gmail.com>
2023-12-11 05:07:21 -0600

    Added more models

--- ClownCollegeWD/Migrations/20231211060917_Complexity.Designer.cs
+++ ClownCollegeWD/Migrations/20231211060917_Complexity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ClownCollegeWD.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClownCollegeWD.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20231211060917_Complexity")]
+    partial class Complexity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
