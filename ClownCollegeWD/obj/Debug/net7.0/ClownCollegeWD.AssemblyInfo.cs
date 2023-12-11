b1d7fdff60a1acfb36aeb4a818eb39a3e8184d86
mason-pAoLo <masonmillionaire725@gmail.com>
2023-12-11 05:54:01 -0600

    Updated Instructors Pages

--- ClownCollegeWD/obj/Debug/net7.0/ClownCollegeWD.AssemblyInfo.cs
+++ ClownCollegeWD/obj/Debug/net7.0/ClownCollegeWD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClownCollegeWD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d6d74f4038e184ad9ff9d5255bfdb7aca5d8d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6513b8ce4e0a01203263684f9ddb21169b7e4fa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClownCollegeWD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClownCollegeWD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
